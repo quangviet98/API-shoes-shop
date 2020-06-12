d53f3d2160499e6d47ecfcbcef344478253fd014
Quang Việt <viettrinh13101998@gmail.com>
2020-06-12 10:59:20 +0700

    make it better

--- middleware/checkAuth.js
+++ middleware/checkAuth.js
@@ -3,7 +3,7 @@ const jwt = require('jsonwebtoken');
 module.exports = (req, res, next) => {
     if (req.headers && req.headers.authorization && req.headers.authorization.split(' ')[0] === 'JWT') {
         const token = req.headers.authorization.split(' ')[1];
-        jwt.verify(token, process.env.JWT_kEY || "secrect", function (err, decoded) {
+        jwt.verify(token, process.env.JWT_kEY || "secret", function (err, decoded) {
             if (err) return res.status(401).json({
                 error: err,
                 message: 'Auth failed!'
