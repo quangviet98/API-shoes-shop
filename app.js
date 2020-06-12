21d4d245921be7eea53d121ffed14b16cfa0935f
Quang Việt <viettrinh13101998@gmail.com>
2020-06-12 16:34:53 +0700

    make it better

--- app.js
+++ app.js
@@ -12,38 +12,39 @@ const productRoutes = require('./api/routes/product');
 const billRoutes = require('./api/routes/bill');
 
 mongoose.connect(`mongodb+srv://viettrinh98:${process.env.pw}@shoes-shop-wiutx.mongodb.net/SHOES?retryWrites=true&w=majority`,
-            {
-                useNewUrlParser : true,
-                useCreateIndex : true,
-                useUnifiedTopology: true
-            })
-            .then(
-                console.log('connect success!')
-            ).catch(err=>{
-                console.log(err);
-            })
+    {
+        useNewUrlParser: true,
+        useCreateIndex: true,
+        useUnifiedTopology: true
+    })
+    .then(
+        console.log('connect success!')
+    ).catch(err => {
+        console.log(err);
+    })
 mongoose.Promise = global.Promise;
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(morgan('dev'));
 app.use(cors());
+app.use('/uploads', express.static('uploads'));
 
-app.use('/api/customers',customerRoutes);
-app.use('/api/feedbacks',feedbackRoutes);
+app.use('/api/customers', customerRoutes);
+app.use('/api/feedbacks', feedbackRoutes);
 app.use('/api/types', typeRoutes);
 app.use('/api/products', productRoutes);
 app.use('/api/bills', billRoutes);
 
-app.use((req, res, next)=>{
+app.use((req, res, next) => {
     const error = new Error('URL not found!');
     error.status = 404;
     next(error);
 })
-app.use((err, req, res, next)=>{
+app.use((err, req, res, next) => {
     res.status(err.status || 500);
     res.json({
-        error : {
-            message : err.message
+        error: {
+            message: err.message
         }
     })
 })
