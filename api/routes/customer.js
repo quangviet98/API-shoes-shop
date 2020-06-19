2e1cef17d44fc7cf8aede6170cc7c1e287dd9016
Quang Việt <viettrinh13101998@gmail.com>
2020-06-19 21:00:12 +0700

    make it better

--- api/routes/customer.js
+++ api/routes/customer.js
@@ -7,7 +7,7 @@ router.get('/', customerController.get_all_customer);
 router.get('/currentPage=:currentPage&limitPerPage=:limitPerPage', customerController.get_customer_pagination);
 router.get('/:id', customerController.get_one_customer);
 router.patch('/reset/:id', checkAuth, customerController.patch_reset);
-router.patch('/:id', customerController.patch_update);
+router.patch('/:id', checkAuth, customerController.patch_update);
 router.post('/signup', customerController.post_signUp);
 router.post('/signin', customerController.post_signin);
 
